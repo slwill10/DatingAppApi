6c99776097a9f790854e6ca8e03b83f0a9568fb0
Willian Santos <wsantosdelima44@gmail.com>
2024-01-09 11:11:59 -0300

    Creating an server controller for testing errors

--- Controllers/BaseApiController.cs
+++ Controllers/BaseApiController.cs
@@ -6,14 +6,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers 
+namespace API.Controllers
 {
     [ApiController]
-[Route("api/[controller]")] 
+    [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-
-        
-
     }
 }
--- DTOs/RegisterDto.cs
+++ DTOs/RegisterDto.cs
@@ -10,6 +10,7 @@ public class RegisterDto
     public string Username {get; set;}
     
     [Required]
+    [StringLength(8, MinimumLength = 4)]
     public string password {get; set;}
 
 }
